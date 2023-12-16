85bd76f9aea18cb3ae6751eff712b99524cd378f
Nkwachi Abel <abelnkwachipromise@yahoo.com>
2023-12-16 18:54:59 +0000

    updated dependencies 	modified:   Homepage.py 	modified:   visuals_prom.py

--- Homepage.py
+++ Homepage.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
-import plotly.graph_objects as go
 from prom_functions import *
 from visuals_prom import *
 
--- visuals_prom.py
+++ visuals_prom.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from streamlit_agraph import agraph, Node, Edge, Config
 import plotly.express as px
 import plotly.graph_objects as go
 import graphviz
@@ -207,113 +206,6 @@ def process_flow_duration(start_activity_df, end_activity_df,process_details_df,
         g.edge(str(row[l_activity]), str(row[end]), label= "  " + count)
 
     st.graphviz_chart(g, use_container_width=True)
-
-def process_flow_agraph(start_activity_df, end_activity_df, process_details_df, graph_count_df, start='Start', end='End', activity='Activity', f_activity='First_Activity', l_activity='Last_Activity'):
-    nodes = []
-    edges = []
-    added_nodes = set()
-
-    max_node = 3
-    max_case_id = process_details_df['Duration'].max()
-
-    # Function to safely add nodes (avoid duplicates)
-    def add_node(node_id, label, color=None):
-        if node_id not in added_nodes:
-            nodes.append(Node(id=node_id, label=label, color=color))
-            added_nodes.add(node_id)
-
-    # Add start activities
-    for index, row in start_activity_df.iterrows():
-        node_id = row[f_activity]
-        node_label = f"{node_id}\n{start_activity_df[start_activity_df[f_activity] == node_id]['Count'].sum()}"
-        add_node(node_id, node_label, color='green')
-        edges.append(Edge(source=start, target=node_id, label=f"  {row['Count']}"))
-
-    # Process details
-    for index, row in process_details_df.iterrows():
-        start_node = row[activity]
-        end_node = row[activity+'_2']
-        start_label = f"{start_node}\n{graph_count_df[graph_count_df[activity] == start_node]['Count'].sum()}"
-        end_label = f"{end_node}\n{graph_count_df[graph_count_df[activity] == end_node]['Count'].sum()}"
-        add_node(start_node, start_label)
-        add_node(end_node, end_label)
-        edges.append(Edge(source=start_node, target=end_node, label=f"  {row['Duration']} day(s)", weight=int(row['Duration'])/max_case_id*max_node+1))
-
-    # Add end activities
-    for index, row in end_activity_df.iterrows():
-        node_id = row[l_activity]
-        node_label = f"{node_id}\n{graph_count_df[graph_count_df[activity] == node_id]['Count'].sum()}"
-        add_node(node_id, node_label, color='red')
-        edges.append(Edge(source=node_id, target=end, label=f"  {row['Count']}"))
-
-    agraph(nodes=nodes, edges=edges, config={'height': 500, 'width': 700, 'directed': True})
-
-def create_agraph(df):
-    max_node = 5
-    max_case_id = df['Count'].max()
-
-    nodes = []
-    edges = []
-
-
-    for idx, row in df.iterrows():
-        node11, node22, weight2 = [str(i) for i in row]
-
-        if node11 not in nodes:
-            nodes.append(Node(id=node11, label=node11))
-            # nodes.add(node11)
-        if node22 not in nodes:
-            nodes.append(Node(id=node22, label=node22))
-            # nodes.add(node22)
-
-        edges.append(Edge(source=node11, target=node22, label=weight2, weight=int(weight2)/max_case_id*max_node))
-
-    # Display the graph
-    return_value = agraph(nodes=nodes, edges=edges, config={'height': 600, 'width': 800, 'directed': True})
-    return return_value
-
-def process_graph_agraph(start_act_df, process_details_df, end_act_df, graph_count_df, activiti_column, rankdir='TB'):
-    nodes = []
-    edges = []
-    node_ids = set()
-
-    for idx, row in start_act_df.iterrows():
-        node1, node2, weight1 = row['Start'], row['First_Activity'], row['Count']
-
-        if node1 not in node_ids:
-            nodes.append(Node(id=node1,label=node1 + '\n' + str(graph_count_df[graph_count_df[activiti_column]== row['Start']]['Count'].sum()), title=node1, color='green', shape='circle', font = '14px arial green', x=-60, y=-500))#fixed=True, 
-            node_ids.add(node1)
-        if node2 not in node_ids:
-            nodes.append(Node(id=node2,label=node2 + '\n' + str(graph_count_df[graph_count_df[activiti_column]== row['First_Activity']]['Count'].sum()), title=node2, shape='box'))
-            node_ids.add(node2)
-
-        edges.append(Edge(source=node1, target=node2, weight=weight1, type="CURE_SMOOTH"))
-
-    for idx, row in process_details_df.iterrows():
-        node11, node22, weight11 = row[activiti_column], row[activiti_column+'_2'], row['Count']
-
-        if node11 not in node_ids:
-            nodes.append(Node(id=node11,label=node11 + '\n' + str(graph_count_df[graph_count_df[activiti_column]== row[activiti_column]]['Count'].sum()), title=node11, shape='box'))
-            node_ids.add(node11)
-        if node22 not in node_ids:
-            nodes.append(Node(id=node22,label=node22 + '\n' + str(graph_count_df[graph_count_df[activiti_column]== row[activiti_column+'_2']]['Count'].sum()), title=node22, shape='box', shapeProperties={'borderRadius':10}))
-            node_ids.add(node22)
-        edges.append(Edge(source=node11, target=node22, label=weight11, smooth={'type':'cubicBezier', 'roundness':1}, font = {'size': '14px', 'color':'red', 'face':'arial'})) #type="CURVE_SMOOTH",
-
-    for idx, row in end_act_df.iterrows():
-        node111, node222, weight111 = row['Last_Activity'], row['End'], row['Count']
-
-        if node111 not in node_ids:
-            nodes.append(Node(id=node111,label=node111 + '\n' + str(graph_count_df[graph_count_df[activiti_column]== row['End']]['Count'].sum()), title=node111, shape='box'))
-            node_ids.add(node111)
-        if node222 not in node_ids:
-            nodes.append(Node(id=node222,label=node222 + '\n' + str(graph_count_df[graph_count_df[activiti_column]== row['Last_Activity']]['Count'].sum()), title=node222, color='red', shape='doublecircle', font = '14px arial red', style='filled'))
-            node_ids.add(node222)
-
-        edges.append(Edge(source=node111, target=node222, weight=weight111, type="CURE_SMOOTH"))
-    configur=Config(width=1200, height=1000, directed=True, graphviz_config = {"rankdir": rankdir})
-    return_value = agraph(nodes=nodes, edges=edges, config=configur)
-    return return_value
 
 def vertical_bar_case_duration(df, x_col,y_col,label,medianduration, color_graph="", vline_text=''):
         fig = px.bar(
